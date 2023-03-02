8a3ad1428fb8252193d36608088475a2426bd4bb
drearyplane8 <chasjoemail@gmail.com>
2023-03-02 12:21:10 +0000

    added form and listener, renamed data file

---
+++ picker.js
@@ -0,0 +1,13 @@
+const dataFile = new File("data.csv")
+let reader = new FileReader()
+
+
+const form = document.getElementById("form")
+
+form.addEventListener("submit", (event) => {
+
+
+    event.preventDefault()
+    
+
+})
